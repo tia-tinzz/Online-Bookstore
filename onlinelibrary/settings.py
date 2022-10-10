31011be2dee0bfc3ce4dacf69579e6c3d8c2daa2
tia-tinzz <tinithomas27@gmail.com>
2022-10-10 18:22:15 +0530

    modified social login

--- onlinelibrary/settings.py
+++ onlinelibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5_7faz7m6pie2vxo@%i+cr7ur4sj@!io$nj=@c5ft)+4cw+kwo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
